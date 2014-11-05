9dfd2acc61475e63ad5e5efbc444f448bf0e7a97
Chioke Harris <chioke.harris@gmail.com>
2014-11-05 18:38:36 -0500

    Add tests for measures JSON file
    At the moment, this file includes comments as pseudocode with some initial thoughts on how the JSON file tests should work.

---
+++ test_measures.py
@@ -0,0 +1,14 @@
+# test measures for validity
+
+# slurp json file
+
+# confirm microsegment data
+###### I'm not entirely sure how this checking should be tiered, since most categories depend on at least one other
+	# confirm valid end use
+		# confirm valid equipment class(es) given end use
+	# confirm valid fuel type (for end use)
+	# confirm 
+
+# confirm presence of needed support fields for each category
+	# sources
+	# units
