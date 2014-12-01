9842b12a120d12a537001bf7aba0156bfa3bda5d
Chioke Harris <chioke.harris@gmail.com>
2014-12-01 14:03:51 -0500

    Added lines to import measures file
    Updated the code with an explanatory opening comment and a few lines to read the measures JSON file, print it to the console, and close the file.

--- test_measures.py
+++ test_measures.py
@@ -1,11 +1,28 @@
 #!/usr/bin/env python3
 
-# test measures for validity
+"""Test JSON measures for validity prior to importing them"""
 
-# slurp json file
+# Import needed packages
+import json
+from pprint import pprint # for testing
+
+
+# Open the database of measures for reading
+json_file = open('measures.json') # need to generalize this to find the json file??
+
+# run a json syntax validator first?
+
+# Read JSON file
+measures = json.load(json_file)
+pprint(measures) # temporary
+
+# Close JSON file
+json_file.close()
 
 # confirm microsegment data
-###### I'm not entirely sure how this checking should be tiered, since most categories depend on at least one other
+######
+###### not entirely sure how this checking should be tiered, since most categories depend on at least one other
+######
 	# confirm valid end use
 		# confirm valid equipment class(es) given end use
 	# confirm valid fuel type (for end use)
