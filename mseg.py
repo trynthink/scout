7cdb27171677525297c81c57bf42c462de6179c4
Jared Langevin <jared.langevin@gmail.com>
2015-02-01 23:25:23 -0500

    Microsegment Updating Routine (Residential)
    This program reads in market microsegment names from the
    microsegments.json file and updates the energy use projections attached
    to each microsegment using the most recent EIA Annual Energy Outlook
    source information.
    
    The file has been developed initially around the residential sector,
    but may be adapted to the commercial sector.  It has been tested on a
    reduced version of the microsegments.json file (residential only, for
    one census division and all building types, fuels, end uses, and
    technologies).  The test runs in about 7 minutes.  Note that the
    program does not currently write out an updated json file, but will do
    so in the future.  Note also that formal tests for each of the
    functions included in this file will be written into the mseg_test.py
    file in the coming days.
    
    A few key points about the code:
    
    1.) A series of dicts are created to translate microsegment names in
    the microsegments.json file into filtering information used to find
    corresponding energy use projections for each microsegment in the AEO
    .txt file.
    
    2.) The main function (“value_replacer_main”) first imports the
    microsegments.json and AEO .txt files.  The json file is read in as a
    dict and the .txt file is read in as a list with each of its rows as
    list elements.  The function then runs through every microsegment key
    in the dict, finds the set of lines in the .txt containing energy use
    information for that microsegment, and stores the energy use
    information for those lines as a list.  This list is then used to
    update the values attached to each microsegment key in the dict.
    Currently, each microsegment list should contain 32 values
    (corresponding to 32 years of energy projections in AEO per
    microsegment, 2009-2040).
    
    3.)  Key supporting functions for “value_replacer_main” include
    “json_translator” (translates microsegment names in the json to row
    filtering information for running through the .txt); and
    “value_listfinder” (finds the rows in the .txt corresponding to each
    microsegment in the json and stores the energy use projections from
    those rows as a list).

--- mseg.py
+++ mseg.py
@@ -1,19 +1,238 @@
 #!/usr/bin/env python3
 
-""" Import and process microsegment data and export to JSON """
+import sys
+import re
+import numpy
+import json
 
-# Import needed packages
+# Set AEO time horizon in years (used in "value_listfinder" function below to check correctness of microsegment value lists)
+aeo_years = 32
+
+# Define a series of dicts that will translate imported JSON microsegment name to AEO microsegment(s)
+
+# Census division dict
+cdivdict = {'new england':1, \
+'mid atlantic':2, \
+'east north central':3, \
+'west north central':4, \
+'south atlantic':5, \
+'east south central':6, \
+'west south central':7, \
+'mountain':8, \
+'pacific':9, \
+'NA': '', \
+}
+
+# Building type dict (residential)
+bldgtypedict = {'single family home':1,'multi family home':2,'mobile home':3,'all/NA':''}
+
+# Fuel type dict    
+fueldict = {'electricity (on site)':'SL','electricity (grid)':'EL', \
+'natural gas':'GS', \
+'distillate':'DS', \
+'other':('LG','KS','CL','SL','GE','NG','WD'), \
+'NA':'' \
+}  # Note that currently in RESDBOUT.txt, electric resistance heaters are categorized under GE (geothermal) fuel. SL (solar) fuel is attached to water heating, but the end technology is only listed as "Solar", which will not currently be included in our analysis.  
 
 
-def grouper(prev_line, curr_line, consume, eqstock):
-    """ Combine data or create new data vectors where appropriate """
+# End use dict
+endusedict = {'heating':'HT','secondary heating':('SH','OA'),'cooling':'CL', \
+'fans & pumps':'FF', \
+'ceiling fan':'CFN', \
+'lighting':'LT', \
+'water heating':'HW', \
+'refrigeration':'RF', \
+'cooking':'CK', \
+'drying':'DR', \
+'TVs':{'TV':'TV','set top box':'STB','DVD':'DVD','home theater & audio':'HTS','video game consoles':'VGC'}, \
+'computers':{'desktop PC':'DPC','laptop PC':'LPC','monitors':'MON','network equipment':'NET'}, \
+'other (grid electric)':{'clothes washing':'CW','dishwasher':'DW','freezers':'FZ','other MELs': ('BAT','COF','DEH','EO','FP','MCO','OA','PHP','SEC','SPA')}, \
+'other (non electric)':{'fuel pumps':'FP'}, \
+'NA':'' , \
+}
 
-    # Fragile to changes in the column definition in the input file
-    if curr_line[0:5] == prev_line[0:5]:
-        eqstock.append(curr_line[6])
-        consume.append(curr_line[-1])
-    return (consume, eqstock)
+# Technology types (supply) dict 
+technology_supplydict = {'solar WH':'SOLAR_WH', \
+'boiler (electric)':'ELEC_RAD','ASHP':'ELEC_HP','GSHP':'GEO_HP', \
+'central AC':'CENT_AIR','room AC':'ROOM_AIR', \
+'linear fluorescent':'LFL','general service':'GSL','reflector':'REF','external':'EXT', \
+'furnace (NG)':'NG_FA','boiler (NG)':'NG_RAD','NGHP':'NG_HP', \
+'furnace (distillate)':'DIST_FA','boiler (distillate)':'DIST_RAD', \
+'furnace (kerosene)':'KERO_FA','furnace (LPG)':'LPG_FA','stove (wood)':'WOOD_HT','resistance':'GE2', \
+'secondary heating (kerosene)':'KS','secondary heating (LPG)':'LG','secondary heating (wood)':'WD', 'secondary heating (coal)':'CL', \
+'NA':'' \
+}
 
+# Technology types (demand) dict 
+technology_demanddict = {'windows conduction':'WIND_COND', 'windows solar':'WIND_SOL', \
+'wall': 'WALL', \
+'roof': 'ROOF', \
+'ground' : 'GRND', \
+'infiltration' : 'INFIL', \
+'people gain' : 'PEOPLE', \
+'equipment gain' : 'EQUIP' \
+}
+
+def json_translator(msdata):
+    """ Determine filtering list for finding information in .txt file parse """
+    # Translate a heating/cooling demand technology case into a filter list
+    if 'demand' in msdata:
+        return [endusedict[msdata[3]],cdivdict[msdata[0]], bldgtypedict[msdata[1]], fueldict[msdata[2]], technology_demanddict[msdata[6]],'demand']
+    # Translate a heating/cooling supply technology case into a filter list
+    elif 'supply' in msdata:
+        return [endusedict[msdata[3]],cdivdict[msdata[0]], bldgtypedict[msdata[1]], fueldict[msdata[2]], technology_supplydict[msdata[6]]]
+    # Translate an end sub use case into a filter list
+    elif msdata[4] is not 'NA':
+        return [endusedict[msdata[3]][msdata[4]],cdivdict[msdata[0]], bldgtypedict[msdata[1]], fueldict[msdata[2]], technology_supplydict[msdata[5]]]
+    # Translate all other technologies into a filter lis
+    else:
+        return [endusedict[msdata[3]], cdivdict[msdata[0]], bldgtypedict[msdata[1]], fueldict[msdata[2]], technology_supplydict[msdata[5]]]
+
+
+def value_listfinder_filterformat(txt_filter):
+    """ Given a filtering list for a microsegment, format into a string that can be entered into a regex comparsion in value_listfinder function """
+    # Set base comparefrom string
+    comparefrom = '.*'
+    for element in txt_filter: # Run through all elements of the filtering list  
+        # If element is a tuple, join the tuple into a single string, put brackets around it for regex comparison
+        if isinstance(element, tuple):
+            newelement = ' |'.join(element)
+            comparefrom = comparefrom + '(' + newelement + ' ).+'
+        # If element is a number, turn into a string for regex comparison
+        elif isinstance(element, int):
+            newelement = str(element)
+            comparefrom = comparefrom + newelement + '.+'
+        # If element is a string, add it to the list without modification
+        elif isinstance(element, basestring):
+            comparefrom = comparefrom + element + '.+'
+        # If element is something else, print error
+        else:
+            print 'Error in list finder form!'
+    return comparefrom
+
+def value_listfinder_listcondense(group_energy):
+    """ Given a list of energy data representing multiple technology types projected out over the AEO time horizon, condense into a list that is length of horizon """
+    # Establish a baseline chunk of projected energy data to add to (i.e., 32 years projected energy data for a given technology)
+    group_energy_base = group_energy[0:aeo_years]
+    # Establish how many chunks of data will be accessed
+    construct_limit = int(float(len(group_energy))/float(aeo_years))
+    # Break original list into chunks and add to new master list that is length of AEO time horizon
+    for newrows in xrange(construct_limit-1):
+        startrow = aeo_years*(newrows+1)
+        endrow = startrow + aeo_years
+        newmat = group_energy[startrow:endrow]
+        group_energy_base = [group_energy_base[i] + newmat[i] for i in xrange(aeo_years)]
+    # Return final list
+    return group_energy_base    
+
+
+def value_listfinder(mstxt_supply, txt_filter): # Note: in the future, will need to add a third input (mstxt_demand) to provide thermal load components data
+    """ Given filtering list for a microsegment, find rows in .txt files to reference in determining associated energy data, append energy data to a new list """   
+    # Define initial energy/(stock?) lists
+    group_energy = []
+    
+    # Run through text file and add all appropriate lines to the empty list
+    # Check whether current microsegment is a heating/cooling "demand" technology (handled differently)
+    if 'demand' in txt_filter:
+        # *** Fill in as we compile demand information ***
+        return 999999999999999 # Use this placeholder for now
+    else:
+        for txtlines in mstxt_supply:
+            # Set up 'compare to' list
+            compareto = str(txtlines)
+            # Set up 'compare from' list (based on .txt file)
+            comparefrom = value_listfinder_filterformat(txt_filter)
+            # Establish the match
+            match = re.search(comparefrom, compareto)
+            # If there's a match, append line to stock/energy lists for given microsegment
+            if match:
+                group_energy.append(txtlines[7])
+        # Given the discovered list of values, check to ensure its length = # of years currently projected by AEO.  If not, execute value_listfinder_listcondense function
+        # to arrive at final list
+        if len(group_energy) is not aeo_years:
+            if len(group_energy)%aeo_years == 0:             
+                group_energy = value_listfinder_listcondense(group_energy)     
+            else:
+                print 'Error in length of discovered list!'
+        # Return updated group_energy
+        return group_energy
+
+# *** The below function is handled in the function above ***
+
+# def grouper(prev_line, curr_line, consume, eqstock):
+#     """ Combine data or create new data vectors where appropriate """
+#     # Fragile to changes in the column definition in the input file
+#     if curr_line[0:5] == prev_line[0:5]:
+#         eqstock.append(curr_line[6])
+#         consume.append(curr_line[-1])
+#     return (consume, eqstock)
+
+def value_replacer_listassemble(mstxt_supply,filterdata):
+    """ Given filtering information from JSON, translate to filter info for .txt and find a list of energy values from .txt given filter """
+    print filterdata
+    # Find the corresponding txt filtering information
+    txt_filter= json_translator(filterdata)
+    print txt_filter
+    # For given microsegment in txt, find a list of energy use projections
+    return value_listfinder(mstxt_supply,txt_filter)
+
+def value_replacer_main():
+    """ Import .txt and JSON files; run through JSON objects; find analogous .txt information; replace JSON values; update JSON """
+    args = sys.argv[1:]    
+    txtfile = args[0]
+    jsonfile = args[1]
+    # Import .txt file    
+    mstxt_supply = numpy.genfromtxt(txtfile, names=True, delimiter='\t', dtype=None)
+    # Import JSON file and run through updating scheme   
+    with open(jsonfile,'r') as js:
+        msjson = json.load(js)
+        # Run through JSON objects, determine replacement information to mine from .txt file, and make the replacement
+        # Census level
+        cdivkeys = msjson.keys()
+        filterdata = []
+        # Building level
+        for cdiv in cdivkeys:
+            bldg = msjson[cdiv]
+            bldgtypekeys = bldg.keys()    
+            # Fuel level
+            for bldgtype in bldgtypekeys:
+                fuel = bldg[bldgtype]
+                fueltypekeys = fuel.keys()
+                # End use level
+                for fueltype in fueltypekeys:
+                    enduse = fuel[fueltype]
+                    endusekeys = enduse.keys()
+                    # Technology level
+                    for endusetype in endusekeys:
+                        tech = enduse[endusetype] 
+                        # Check whether there are more levels for given microsegment, if not end loop
+                        if tech:
+                            techkeys = tech.keys()
+                            # Heating/cooling technology sub-level
+                            for techtype in tech:
+                                heatcooltech = tech[techtype]  
+                                # Check whether there are more levels for given microsegment, if not end loop
+                                if heatcooltech:  
+                                    for heatcooltechtype in heatcooltech:
+                                        filterdata = [cdiv, bldgtype,fueltype,endusetype,'NA',techtype,heatcooltechtype]
+                                        # Replace initial json value for microsegment with list
+                                        msjson[filterdata[0]][filterdata[1]][filterdata[2]][filterdata[3]][filterdata[5]][filterdata[6]] = value_replacer_listassemble(mstxt_supply,filterdata)
+                                else:
+                                    # Check whether the given technology is handled as its own end use in AEO (As an example: While our microsegments JSON currently
+                                    # considers DVDs to be a technology type within a "TVs" end use category, AEO handles "DVDs" as an end use)
+                                    if isinstance(endusedict[endusetype],dict):                                   
+                                        # Set subendusetype to the tech type to reconcile JSON/txt difference in handling
+                                        subendusetype = techtype
+                                        filterdata = [cdiv, bldgtype,fueltype,endusetype,subendusetype,'NA','NA']
+                                        msjson[filterdata[0]][filterdata[1]][filterdata[2]][filterdata[3]][filterdata[4]] = value_replacer_listassemble(mstxt_supply,filterdata)
+                                    else:
+                                        filterdata = [cdiv, bldgtype,fueltype,endusetype,'NA',techtype,'NA']
+                                        msjson[filterdata[0]][filterdata[1]][filterdata[2]][filterdata[3]][filterdata[5]] = value_replacer_listassemble(mstxt_supply,filterdata)
+                        else:
+                            filterdata = [cdiv, bldgtype,fueltype,endusetype,'NA','NA','NA']
+                            msjson[filterdata[0]][filterdata[1]][filterdata[2]][filterdata[3]] = value_replacer_listassemble(mstxt_supply,filterdata)
+    # Return the updated json
+    return msjson
 
 if __name__ == '__main__':
-    main()
+    value_replacer_main()
