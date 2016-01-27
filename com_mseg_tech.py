54c3212e17de013055f60ccc7dc4742ad01589fd
Chioke Harris <trynthink@gmail.com>
2016-01-27 17:27:35 -0500

    Revised tech data selection function and tests
    The data originally used to set up the tests for the technology data selection function did not include a representative range of technologies that would be extracted by the function from the full EIA data array. The input and output sample data for the tests were modified accordingly. As a result, the cost extraction function now does not work correctly and the test for that function is commented out. That function will be rebuilt and will incorporate service demand data for the average cost, performance, and lifetime calculations.

--- com_mseg_tech.py
+++ com_mseg_tech.py
@@ -7,6 +7,31 @@ import com_mseg as cm
 import numpy as np
 
 
+def tech_data_selector(tech_data, sel):
+    """ From the full structured array of cost, performance, and
+    lifetime data from the AEO, extract a group of data using numeric
+    indices generated from the text indices at the leaf nodes of the
+    input microsegments JSON. Each group of data extracted by this
+    function will correspond to multiple technologies and performance
+    levels and will require further processing. """
+
+    # Determine whether the data indicated in the 'r' column indicates
+    # building type or census division based on the end use indicated
+    # (building type for ventilation, lighting, and refrigeration)
+    if sel[2] in [4, 6, 7]:
+        tmp = sel[1]  # use building type
+    else:
+        tmp = sel[0]  # use census division
+
+    # Filter technology data based on the specified census
+    # division or building type, end use, and fuel type
+    filtered = tech_data[np.all([tech_data['r'] == tmp,
+                                 tech_data['s'] == sel[2],
+                                 tech_data['f'] == sel[3]], axis=0)]
+
+    return filtered
+
+
 def cost_extractor(tech_array, years):
     """ From a numpy structured array of data for a single technology
     with several rows corresponding to different performance levels,
@@ -52,31 +77,6 @@ def cost_extractor(tech_array, years):
                   'best': dict(zip(map(str, years), cost_max))}
 
     return final_dict
-
-
-def tech_data_selector(tech_data, sel):
-    """ From the full structured array of cost, performance, and
-    lifetime data from the AEO, extract a group of data using numeric
-    indices generated from the text indices at the leaf nodes of the
-    input microsegments JSON. Each group of data extracted by this
-    function should correspond to all of the performance levels given
-    for a single technology, and (crucially) no other technologies. """
-
-    # Determine whether the data indicated in the 'r' column indicates
-    # building type or census division based on the end use indicated
-    # (building type for ventilation, lighting, and refrigeration)
-    if sel[2] in [4, 6, 7]:
-        tmp = sel[1]  # use building type
-    else:
-        tmp = sel[0]  # use census division
-
-    # Filter technology data based on the specified census
-    # division or building type, end use, and fuel type
-    filtered = tech_data[np.all([tech_data['r'] == tmp,
-                                 tech_data['s'] == sel[2],
-                                 tech_data['f'] == sel[3]], axis=0)]
-
-    return filtered
 
 if __name__ == '__main__':
     main()
