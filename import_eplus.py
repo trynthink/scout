d53114aa1ab0de3a605be9f153b3d3830c314876
Jared Langevin <jared.langevin@gmail.com>
2016-02-25 12:43:38 -0500

    Derive weights for mapping EPlus vintages to Scout 'structure type'
    Measure performance data coming from EnergyPlus/OpenStudio will be
    broken down by several building vintage types. These vintages must be
    mapped to the ‘new’ and/or ‘retrofit’ building structure type that is
    defined for commercial measures in Scout.
    
    This commit uses building vintage square footage data available from
    CBECS 2012 (http://www.eia.gov/consumption/commercial/data/2012/) to
    develop the EnergyPlus building vintage weights that will be needed for
    the above mapping exercise.
    
    It is assumed that the most recent EnergyPlus building vintage
    corresponds to the Scout ‘new’ structure type; therefore, all data
    associated with that newest EnergyPlus vintage will be assigned a
    weight of 1 (reflecting the need to map only 1 EnergyPlus vintage
    category).
    
    Data associated with all other EnergyPlus vintages must be weighted
    such that they may be combined to represent the ‘retrofit’ structure
    type assigned to Scout measures. These weights are determined by
    assigning a certain square footage to each EnergyPlus retrofit vintage
    category (based on CBECS) and dividing that square footage across all
    EnergyPlus retrofit vintage categories.
    
    Tests have been developed (‘import_eplus_test.py’) to ensure proper
    reading in of CBECS square footage data and subsequent determination of
    EnergyPlus building vintage weights.

---
+++ import_eplus.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python3
+import xlrd
+import numpy
+import re
+
+# Define CBECS square footage file
+CBECS_in = "b34.xlsx"
+# Define EnergyPlus data file
+EPlus_perf_in = "advanced_hybrid_rtus.xlsx"
+
+
+def CBECS_vintage_sf(CBECS_sh):
+    """ Import commercial floorspace data by vintage from CBECS
+    'b34.xlsx' raw data file """
+
+    # Initialize a dictionary for the CBECS vintage square footages
+    vintage_sf = {}
+    # Initialize a flag for the first relevant row in the CBECS Excel sheet
+    start_row_flag = 0
+
+    # Loop through all rows in the Excel sheet, and pull out sq.ft. data
+    for i in range(CBECS_sh.nrows):
+        # Check for a string that indicates the start of the sq.ft. data rows
+        if CBECS_sh.cell(i, 0).value == "Year constructed":
+            start_row_flag = 1
+        # Check for a string that indicates the end of sq.ft. data rows (break)
+        elif CBECS_sh.cell(i, 0).value == "Census region and division":
+            break
+        # If start row flag hasn't been raised, skip row
+        elif start_row_flag == 0:
+            continue
+        # If start row flag has been raised, read in sq.ft. data from row
+        else:
+            vintage_bin = CBECS_sh.cell(i, 0).value
+            sf_val = CBECS_sh.cell(i, 6).value
+            vintage_sf[vintage_bin] = sf_val
+
+    # If the start row flag was never raised, yield an error
+    if start_row_flag == 0:
+        raise ValueError('Problem reading in the CBECS sq.ft. data!')
+
+    # Return the square footage information
+    return vintage_sf
+
+
+def find_vintage_weights(vintage_sf, EPlus_vintages):
+    """ Use CBECS building vintage square footage data to derive weighting
+    factors that will map the EnergyPlus building vintages to the 'new'
+    and 'retrofit' building structure types of Scout """
+
+    # Set the expected names of the EnergyPlus building vintages and the
+    # low and high year limits of each building vintage category
+    expected_EPlus_vintage_yr_bins = {
+        '90.1-2004': [2004, 2006], '90.1-2007': [2007, 2009],
+        '90.1-2010': [2010, 2012], '90.1-2013': [2013, 2016],
+        'DOE Ref 1980-2004': [1980, 2003], 'DOE Ref Pre-1980': [0, 1979]}
+    # Set the expected EnergyPlus building vintage year that will correspond
+    # to a 'new' (as opposed to 'retrofit') building structure type in Scout
+    expected_EPlus_new_vintage = '90.1-2013'
+    # Initialize a variable meant to translate the summed square footages of
+    # of multiple 'retrofit' building vintages into weights that sum to 1; also
+    # initialize a variable used to check that these weights indeed sum to 1
+    total_retro_sf, retro_weight_sum = (0 for n in range(2))
+
+    # Check for expected EnergyPlus vintage names; if there are unexpected
+    # vintage names, raise an error
+    if sorted(EPlus_vintages) == sorted(expected_EPlus_vintage_yr_bins.keys()):
+        # Initialize a dictionary with the EnergyPlus vintages as keys and
+        # associated square footage values starting at zero
+        eplus_vintage_weights = dict.fromkeys(EPlus_vintages, 0)
+
+        # Loop through the EnergyPlus vintages and assign associated weights
+        # by mapping to CBECS square footage data
+        for k in eplus_vintage_weights.keys():
+            # If looping through the EnergyPlus vintage associated with the
+            # 'new' Scout structure type, set vintage weight to 1 (only one
+            # vintage category will be associated with this structure type)
+            if k == expected_EPlus_new_vintage:
+                eplus_vintage_weights[k] = 1
+            # Otherwise, set EnergyPlus vintage weight initially to the
+            # square footage that corresponds to that vintage in CBECS
+            else:
+                # Loop through all CBECS vintage bins
+                for k2 in vintage_sf.keys():
+                    # Find the limits of the CBECS vintage bin
+                    cbecs_match = re.search(
+                        '(\D*)(\d*)(\s*)(\D*)(\s*)(\d*)', k2)
+                    cbecs_t1 = cbecs_match.group(2)
+                    cbecs_t2 = cbecs_match.group(6)
+                    # Handle a 'Before Year X' case in CBECS (e.g., 'Before
+                    # 1920'),  setting the lower year limit to zero
+                    if cbecs_t2 == '':
+                        cbecs_t2 = 0
+                    # Determine a single average year that represents the
+                    # current CBECS vintage bin
+                    cbecs_yr = (int(cbecs_t1) + int(cbecs_t2)) / 2
+                    # If the CBECS bin year falls within the year limits of the
+                    # current EnergyPlus vintage bin, add the associated
+                    # CBECS sq.ft. data to the EnergyPlus vintage weight value
+                    if cbecs_yr >= expected_EPlus_vintage_yr_bins[k][0] and \
+                       cbecs_yr < expected_EPlus_vintage_yr_bins[k][1]:
+                        eplus_vintage_weights[k] += vintage_sf[k2]
+                        total_retro_sf += vintage_sf[k2]
+
+        # Run through all EnergyPlus vintage weights, normalizing the square
+        # footage-based weights for each 'retrofit' vintage to the total square
+        # footage across all 'retrofit' vintage categories
+        for k in eplus_vintage_weights.keys():
+            # If running through the 'new' EnergyPlus vintage bin, register
+            # the value of its weight (should be 1)
+            if k == expected_EPlus_new_vintage:
+                new_weight_sum = eplus_vintage_weights[k]
+            # If running through a 'retrofit' EnergyPlus vintage bin,
+            # normalize the square footage for that vintage by total
+            # square footages across 'retrofit' vintages to arrive at the
+            # final weight for that EnergyPlus vintage
+            else:
+                eplus_vintage_weights[k] /= total_retro_sf
+                retro_weight_sum += eplus_vintage_weights[k]
+
+        # Check that the 'new' EnergyPlus vintage weight equals 1 and that
+        # all 'retrofit' EnergyPlus vintage weights sum to 1
+        if new_weight_sum != 1:
+            raise ValueError('Incorrect new vintage weight total!')
+        elif retro_weight_sum != 1:
+            raise ValueError('Incorrect retrofit vintage weight total!')
+
+    else:
+        raise KeyError((
+            'Unexpected EPlus vintage(s)! '
+            'Adjust EPlus vintage assumptions in find_vintage_weights'))
+
+    # Return the weights needed to map each EnergyPlus vintage category to the
+    # 'new' and 'retrofit' structure types defined in Scout
+    return eplus_vintage_weights
+
+
+def main():
+
+    # Import CBECS XLSX
+    CBECS = xlrd.open_workbook(CBECS_in)
+    CBECS_sh = CBECS.sheet_by_index(0)
+    # Pull out building vintage square footage data
+    vintage_sf = CBECS_vintage_sf(CBECS_sh)
+
+    # Import EnergyPlus measure performance XLSX
+    EPlus_perf = xlrd.open_workbook(EPlus_perf_in)
+    EPlus_perf_sh = EPlus_perf.sheet_by_index(2)
+    # Determine appropriate weights for mapping EnergyPlus vintages to the
+    # 'new' and 'retrofit' building structure types of Scout
+    EPlus_vintages = [EPlus_perf_sh.cell(x, 2).value for
+                      x in range(1, EPlus_perf_sh.nrows)]
+    EPlus_vintages = numpy.unique(EPlus_vintages)
+    EPlus_vintage_weights = find_vintage_weights(vintage_sf, EPlus_vintages)
+
+if __name__ == "__main__":
+    main()
---
+++ import_eplus_test.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python3
+
+""" Tests for running the EnergyPlus data import """
+
+# Import code to be tested
+import import_eplus
+# Import needed packages
+import unittest
+import xlrd
+import numpy
+
+# Define ok sample CBECS square footage file
+CBECS_in_test_ok = "CBECS_test_ok.xlsx"
+CBECS_ok = xlrd.open_workbook(CBECS_in_test_ok)
+CBECS_sh_ok = CBECS_ok.sheet_by_index(0)
+
+# Define fail sample CBECS square footage file
+CBECS_in_test_fail = "CBECS_test_fail.xlsx"
+CBECS_fail = xlrd.open_workbook(CBECS_in_test_fail)
+CBECS_sh_fail = CBECS_fail.sheet_by_index(0)
+
+# Define ok sample EnergyPlus data file
+EPlus_perf_in_test_ok = "EPlus_test_ok.xlsx"
+EPlus_perf_ok = xlrd.open_workbook(EPlus_perf_in_test_ok)
+EPlus_perf_sh_ok = EPlus_perf_ok.sheet_by_index(2)
+# Determine EnergyPlus vintage names from the above
+EPlus_vintages_ok = [EPlus_perf_sh_ok.cell(x, 2).value for
+                     x in range(1, EPlus_perf_sh_ok.nrows)]
+EPlus_vintages_ok = numpy.unique(EPlus_vintages_ok)
+
+# Define fail sample EnergyPlus data file
+EPlus_perf_in_test_fail = "EPlus_test_fail.xlsx"
+EPlus_perf_fail = xlrd.open_workbook(EPlus_perf_in_test_fail)
+EPlus_perf_sh_fail = EPlus_perf_fail.sheet_by_index(2)
+# Determine EnergyPlus vintage names from the above
+EPlus_vintages_fail = [EPlus_perf_sh_fail.cell(x, 2).value for
+                       x in range(1, EPlus_perf_sh_fail.nrows)]
+EPlus_vintages_fail = numpy.unique(EPlus_vintages_fail)
+
+
+class CommonMethods(object):
+    """ Define common methods for use in all tests below """
+
+    # Create a routine for checking equality of a dict with point vals
+    def dict_check(self, dict1, dict2, msg=None):
+        for (k, i), (k2, i2) in zip(sorted(dict1.items()),
+                                    sorted(dict2.items())):
+            if isinstance(i, dict):
+                self.assertCountEqual(i, i2)
+                self.dict_check(i, i2)
+            else:
+                self.assertAlmostEqual(dict1[k], dict2[k2], places=2)
+
+
+class CBECSVintageSFTest(unittest.TestCase, CommonMethods):
+    """ Test 'CBECS_vintage_sf' function to ensure building vintage
+     square footages are read in properly from a CBECS data file """
+
+    # Output dictionary that should be generated given the above
+    # 'CBECS_sh_ok' input sheet
+    ok_out = {
+        '2004 to 2007': 6524.0, '1960 to 1969': 10362.0,
+        '1946 to 1959': 7381.0, '1970 to 1979': 10846.0,
+        '1990 to 1999': 13803.0, '2000 to 2003': 7215.0,
+        'Before 1920': 3980.0, '2008 to 2012': 5726.0,
+        '1920 to 1945': 6020.0, '1980 to 1989': 15185.0}
+
+    # Test for correct determination of CBECS square footages by
+    # building vintage
+    def test_vintageweights(self):
+        self.dict_check(import_eplus.CBECS_vintage_sf(
+            CBECS_sh_ok), self.ok_out)
+
+    # Test that an error is raised when none of the CBECS XLSX
+    # rows are read in
+    def test_vintageweights_fail(self):
+        with self.assertRaises(ValueError):
+            import_eplus.CBECS_vintage_sf(
+                CBECS_sh_fail)
+
+
+class FindVintageWeightsTest(unittest.TestCase, CommonMethods):
+    """ Test 'find_vintage_weights' function to ensure the proper weights are
+    derived for mapping EnergyPlus building vintages to Scout's 'new' and
+    'retrofit' building structure types """
+
+    # Sample set of CBECS square footage data to map to EnergyPlus vintages
+    sample_sf = {
+        '2004 to 2007': 6524.0, '1960 to 1969': 10362.0,
+        '1946 to 1959': 7381.0, '1970 to 1979': 10846.0,
+        '1990 to 1999': 13803.0, '2000 to 2003': 7215.0,
+        'Before 1920': 3980.0, '2008 to 2012': 5726.0,
+        '1920 to 1945': 6020.0, '1980 to 1989': 15185.0}
+
+    # Output dictionary that should be generated given the above sample square
+    # footage data and 'EPlus_vintages_ok' input sheet
+    ok_out = {
+        'DOE Ref 1980-2004': 0.42, '90.1-2004': 0.07,
+        '90.1-2007': 0.0, '90.1-2010': 0.07,
+        'DOE Ref Pre-1980': 0.44, '90.1-2013': 1}
+
+    # Test for correct determination of vintage weights
+    def test_vintageweights(self):
+        self.dict_check(import_eplus.find_vintage_weights(
+            self.sample_sf, EPlus_vintages_ok), self.ok_out)
+
+    # Test that an error is raised when unexpected EPlus vintages are present
+    def test_vintageweights_fail(self):
+        with self.assertRaises(KeyError):
+            import_eplus.find_vintage_weights(
+                self.sample_sf, EPlus_vintages_fail)
+
+
+# Offer external code execution (include all lines below this point in all
+# test files)
+def main():
+    # Triggers default behavior of running all test fixtures in the file
+    unittest.main()
+
+if __name__ == "__main__":
+    main()
