fde7ed307166c1365980352c9c2e3956d3425f0d
Jared Langevin <jared.langevin@gmail.com>
2016-10-27 15:36:08 -0400

    Separated plots by adoption scenario
    Plots are now separated by adoption scenario (technical potential, max
    adoption potential) instead of competition scenario (uncompeted,
    competed) as before. Uncompeted and competed results for a given
    adoption scenario, plotting variable, and ECM are now shown on the same
    plot. The line weights and colors used in each plot have also been
    adjusted for improved legibility.

--- plots.R
+++ plots.R
@@ -20,6 +20,10 @@ compete_results<-fromJSON(file = paste(base_dir, '/engine_results/meas_engine_ou
 plot_names <- c('Total Energy', 'Total Carbon', 'Total Cost')
 # Set plot axis labels
 plot_axis_labels<-c('Primary Energy Use (Quads)', expression(CO[2] ~" Emissions (MMTons)"), 'Energy Cost (Billion $)')
+# Set plot colors for uncompeted baseline, efficient and low/high results
+plot_col_uc_base = "gray60"
+plot_col_uc_eff = "gray80"
+plot_col_uc_lowhigh = "gray90"
 
 # ============================================================================
 # Set high-level ECM variables
@@ -51,47 +55,59 @@ var_names_compete_eff_h <- c(
   'Efficient Energy Use (high) (MMBtu)', 'Efficient CO₂ Emissions (high) (MMTons)', 'Efficient Energy Cost (high) (USD)') 
 # Initialize a matrix used to store baseline data for the uncompeted plot
 # (to be included for reference in the competed plot)
-base_rec_uncompete = matrix(NA, length(meas_names), length(years))
 
 # ============================================================================
 # Generate plots for total energy, carbon, and cost
 # ============================================================================
 
-# Loop through all plotting variables
-for (v in 1:length(var_names_uncompete)){
+# Loop through all adoption scenarios
+for (a in 1:length(adopt_scenarios)){
 
-  # Set a factor to convert the results data to final plotting units for given variable
-  # (quads for energy, MMT for carbon, and billion $ for cost)
-  if ((var_names_uncompete[v] == "energy") | (var_names_uncompete[v] == "cost")){
-    unit_translate = 1/1000000000 # converts energy from MBtu -> quads or cost from $ -> billion $
-    }else{
-      unit_translate = 1 # carbon results data are already imported in MMT units
-    }
+  # Set plot colors for competed baseline, efficient, and low/high results
+  # (varies by adoption scenario)
+  if (adopt_scenarios[a] == "Technical potential"){
+    plot_col_c_base = "midnightblue"
+    plot_col_c_eff = "lightskyblue"
+    plot_col_c_lowhigh = "lightskyblue"
+  }else{
+    plot_col_c_base = "red3"
+    plot_col_c_eff = "pink"
+    plot_col_c_lowhigh = "lightpink"
+  }
   
-  # Loop through all competition schemes
-  for (cp in 1:length(comp_schemes)){
-    
-    # Initialize uncertainty flag for the competition scheme
-    uncertainty = FALSE
-    # Set the file name for the plot based on the competition scheme
-    if (comp_schemes[cp] == 'uncompeted'){
-      plot_file_name = paste(base_dir, '/engine_results/plots/uncompeted/', plot_names[v],".pdf", sep = "")
+  # Loop through all plotting variables
+  for (v in 1:length(var_names_uncompete)){
+
+    # Set a factor to convert the results data to final plotting units for given variable
+    # (quads for energy, MMT for carbon, and billion $ for cost)
+    if ((var_names_uncompete[v] == "energy") | (var_names_uncompete[v] == "cost")){
+      unit_translate = 1/1000000000 # converts energy from MBtu -> quads or cost from $ -> billion $
       }else{
-        plot_file_name = paste(base_dir, '/engine_results/plots/competed/', plot_names[v],".pdf", sep = "")
+        unit_translate = 1 # carbon results data are already imported in MMT units
       }
-    # Open PDF plot device
-    pdf(plot_file_name,width=13,height=14)
-    # Set number of rows and columns per page in PDF output
-    par(mfrow=c(4,4))
-    # Reconfigure space around each side of the plot for best fit
-    par(mar=c(5.1,5.1,3.1,2.1))
+    
+      # Initialize results vector (3 rows accommodate mean, low, and high results values;
+      # 4 columns accommodate 2 outputs (baseline and efficient) x 2 adoption scenarios)
+      results <- matrix(list(), 3, 2*length(comp_schemes))
+      # Initialize uncertainty flag for the adoption scenario
+      uncertainty = FALSE
+      # Set the file name for the plot based on the adoption scenario and plotting variable
+      if (adopt_scenarios[a] == 'Technical potential'){
+        plot_file_name = paste(base_dir, '/engine_results/plots/tech_potential/', plot_names[v],".pdf", sep = "")
+      }else{
+        plot_file_name = paste(base_dir, '/engine_results/plots/max_adopt_potential/', plot_names[v],".pdf", sep = "")
+      }
+    
+      # Open PDF plot device
+      pdf(plot_file_name,width=13,height=14)
+      # Set number of rows and columns per page in PDF output
+      par(mfrow=c(4,4))
+      # Reconfigure space around each side of the plot for best fit
+      par(mar=c(5.1,5.1,3.1,2.1))
     
     # Loop through all ECMs
     for (m in 1:length(meas_names)){
-      
-      # Initialize results vector (3 rows accommodate mean, low, and high results values;
-      # 4 columns accommodate 2 outputs (baseline and efficient) x 2 adoption scenarios)
-      results <- matrix(list(), 3, 2*length(adopt_scenarios))
+    
       # Find the index for accessing the item in the list of uncompeted results that corresponds
       # to data for the current ECM. Note: competed results are accessed directly by ECM name,
       # and do not require an index
@@ -100,10 +116,10 @@ for (v in 1:length(var_names_uncompete)){
           uc_name_ind = uc
         }
       }
-      
-      # Loop through all adoption scenarios
-      for (a in 1:length(adopt_scenarios)){
-        
+
+      # Loop through all competition schemes
+      for (cp in 1:length(comp_schemes)){
+          
         # Set matrix for temporarily storing finalized baseline and efficient results
         r_temp <- matrix(NA, 4, length(years))  
         # Find data for uncompeted energy, carbon, and/or cost
@@ -162,7 +178,7 @@ for (v in 1:length(var_names_uncompete)){
           }
         }
         # Set the column start and stop indexes to use in updating the matrix of results
-        ind_start = ((a-1)*(length(adopt_scenarios))) + 1
+        ind_start = ((cp-1)*(length(comp_schemes))) + 1
         ind_end = ind_start + 1 # note this accommodates baseline and efficient outcomes
         # Update results matrix with mean, low, and high baseline and efficient outcomes
         results[, ind_start:ind_end] = rbind(cbind(list(r_temp[1,]), list(r_temp[2,])),
@@ -170,100 +186,66 @@ for (v in 1:length(var_names_uncompete)){
                                              cbind(list(r_temp[1,]), list(r_temp[4,])))
       }
 
-      # Set baseline results for each adoption scenario
-      base_tp = unlist(results[1, 1]) * unit_translate
-      base_map = unlist(results[1, 3]) * unit_translate
-      # Set efficient results for each adoption scenario
-      eff_tp_m = unlist(results[1, 2]) * unit_translate
-      eff_map_m = unlist(results[1, 4]) * unit_translate
-      # If applicable, set low and high bounds on the efficient results for each adoption
-      # scenario
-      if (uncertainty == TRUE){
-        # Set low/high technical potential bounds
-        eff_tp_l = unlist(results[2, 2]) * unit_translate
-        eff_tp_h = unlist(results[3, 2]) * unit_translate
-        # Set low/high max adoption potential bounds
-        eff_map_l = unlist(results[2, 4]) * unit_translate
-        eff_map_h = unlist(results[3, 4]) * unit_translate
-      }
+      # Set uncompeted and competed baseline results for given adoption scenario,
+      # plotting variable, and ECM  
+      base_uc = unlist(results[1, 1]) * unit_translate
+      base_c = unlist(results[1, 3]) * unit_translate
+      # Set uncompeted and competed efficient results for adoption scenario,
+      # plotting variable, and ECM (mean and low/high values)
+      eff_uc_m = unlist(results[1, 2]) * unit_translate
+      eff_uc_l = unlist(results[2, 2]) * unit_translate
+      eff_uc_h = unlist(results[3, 2]) * unit_translate
+      eff_c_m = unlist(results[1, 4]) * unit_translate
+      eff_c_l = unlist(results[2, 4]) * unit_translate
+      eff_c_h = unlist(results[3, 4]) * unit_translate
+      
+      # Find the min. and max. values in the data to be plotted
+      min_val = min(c(base_uc, base_c, eff_uc_m, eff_uc_l, eff_uc_h,
+                      eff_c_m, eff_c_l, eff_c_h))
+      max_val = max(c(base_uc, base_c, eff_uc_m, eff_uc_l, eff_uc_h,
+                      eff_c_m, eff_c_l, eff_c_h))
+      # Set limits of y axis for plot based on min. and max. values in data
+      ylims = pretty(c(min_val-0.05*max_val, max_val+0.05*max_val))
 
-      # Initialize plot and legend parameters for an uncompeted scenario
-      if (comp_schemes[cp] == "uncompeted"){
-        # Find min. and max. values in the data to be plotted for setting y axis limits
-        min_val = min(c(base_tp, base_map, eff_tp_m, eff_map_m))
-        max_val = max(c(base_tp, base_map, eff_tp_m, eff_map_m))
-        # Set limits of y axis for plot based on min. and max. values in data
-        ylims = pretty(c(min_val-0.05*max_val, max_val+0.05*max_val))
-        # Initiate plot region with baseline results
-        plot(years, base_tp, typ='l', lwd=4, ylim = c(min(ylims), max(ylims)),
-             xlab=NA, ylab=NA, col="gray30", main = meas_names[m], xaxt="n", yaxt="n")
-        # Set plot legend parameters, which depend on the presence of uncertainty in outputs
-        if (uncertainty == TRUE){
-          legend_param = c("Baseline (Uncompeted)", "Efficient (Uncompeted TP)", "Efficient (Uncompeted MAP)",
-                           "Efficient (Uncompeted TP, 5th/95th PCT)", "Efficient (Uncompeted MAP, 5th/95th PCT)")
-          col_param = c("gray30", "dodgerblue", "firebrick1", "dodgerblue", "firebrick1")
-          lwd_param = c(4, 2.5, 2.5, 1.5, 1.5)
-          lty_param = c(1, 1, 1, 3, 3)
-        }else{
-          legend_param = c("Baseline (Uncompeted)", "Efficient (Uncompeted TP)", "Efficient (Uncompeted MAP)")
-          col_param = c("gray30", "dodgerblue", "firebrick1")
-          lwd_param = c(4, 2.5, 2.5)
-          lty_param = rep(1, 3)
-        }
-        # Record uncompeted baseline data for each ECM; these data will subsequently be added as an uncompeted
-        # baseline reference in each ECM's competed scenario plot, which may include additional adjusted baselines
-        base_rec_uncompete[m, ] = base_tp
+      # Initialize the plot with uncompeted baseline results
+      plot(years, base_uc, typ='l', lwd=5, ylim = c(min(ylims), max(ylims)),
+           xlab=NA, ylab=NA, col=plot_col_uc_base, main = meas_names[m], xaxt="n", yaxt="n")
         
-        # Initialize plot and legend parameters for a competed scenario
-        }else{
-          # Find min. and max. values in the data to be plotted for setting y axis limits
-          min_val = min(c(base_tp, base_map, eff_tp_m, eff_map_m, base_rec_uncompete[m, ]))
-          max_val = max(c(base_tp, base_map, eff_tp_m, eff_map_m, base_rec_uncompete[m, ]))
-          # Set limits of y axis for plot based on min. and max. values in data
-          ylims = pretty(c(min_val-0.05*max_val, max_val+0.05*max_val))
-          # Initiate plot region with adjusted technical potential baseline results
-          plot(years, base_tp, typ='l', lwd=4, ylim = c(min(ylims), max(ylims)),
-               xlab=NA, ylab=NA, col="dodgerblue", main = meas_names[m], xaxt="n", yaxt="n")
-          # Add adjusted max adoption potential baseline results
-          lines(years, base_map, lwd=4, col="firebrick1")
-          # Set plot legend parameters, which depend on the presence of uncertainty in outputs
-          if (uncertainty == TRUE){
-            legend_param = c("Baseline (Uncompeted)", "Baseline (Competed TP)", "Baseline (Competed MAP)",
-                             "Efficient (Competed TP)", "Efficient (Competed MAP)",
-                             "Efficient (Competed TP, 5th/95th PCT)", "Efficient (Competed MAP, 5th/95th PCT)")
-            col_param = c(
-              "gray30", "dodgerblue", "firebrick1", "dodgerblue", "firebrick1", "dodgerblue", "firebrick1")
-            lwd_param = c(2, 4, 4, 2.5, 2.5, 1.5, 1.5)
-            lty_param = c(6, 1, 1, 1, 1, 3, 3)
-          }else{
-            legend_param = c("Baseline (Uncompeted)", "Baseline (Competed TP)", "Baseline (Competed MAP)",
-                             "Efficient (Competed TP)", "Efficient (Competed MAP)")
-            col_param = c("gray30", "dodgerblue", "firebrick1", "dodgerblue", "firebrick1")
-            lwd_param = c(2, 4, 4, 2.5, 2.5)
-            lty_param = c(6, 1, 1, 1, 1) 
-          }  
-        }
-
-      # Add low/high bounds on efficient results (if applicable)
+      # Determine legend parameters based on whether uncertainty is present in results
       if (uncertainty == TRUE){
-        # Technical potential scenario
-        lines(years, eff_tp_l, lwd=1.5, lty=3, col="dodger blue")
-        lines(years, eff_tp_h, lwd=1.5, lty=3, col="dodger blue")
-        # Max adoption potential scenario
-        lines(years, eff_map_l, lwd=1.5, lty=3, col="firebrick1")
-        lines(years, eff_map_h, lwd=1.5, lty=3, col="firebrick1")
-      }      
-
-      # Add mean values of efficient results
-      # Technical potential scenario
-      lines(years, eff_tp_m, lwd=2.5, col="dodgerblue")
-      # Max adoption potential scenario
-      lines(years, eff_map_m, lwd=2.5, col="firebrick1")
-      # In a competed scenario, add uncompeted baseline for reference
-      if (comp_schemes[cp] == "competed"){
-        lines(years, base_rec_uncompete[m, ], lwd=2, col="gray30", lty=6)
+        # Set legend parameters for a plot with uncertainty in the results
+        legend_param = c(
+          "Baseline (Uncompeted)", "Baseline (Competed)",
+          "Efficient (Uncompeted)", "Efficient (Competed)",
+          "Efficient (Uncompeted, 5th/95th PCT)", "Efficient (Competed, 5th/95th PCT)")
+        col_param = c(plot_col_uc_base, plot_col_c_base, plot_col_uc_eff,
+                      plot_col_c_eff, plot_col_uc_lowhigh, plot_col_c_lowhigh)
+        lwd_param = c(5, 3, 3.5, rep(2, 4))
+        lty_param = c(rep(1, 4), rep(3, 2))
+        }else{
+          # Set legend parameters for a plot with no uncertainty in the results
+          legend_param = c("Baseline (Uncompeted)", "Baseline (Competed)",
+                           "Efficient (Uncompeted)", "Efficient (Competed)")
+          col_param = c(plot_col_uc_base, plot_col_c_base, plot_col_uc_eff, plot_col_c_eff)
+          lwd_param = c(5, 3, 3.5, rep(2, 2))
+          lty_param = rep(1, 4)  
+        }
+     
+      # Add low/high bounds on uncompeted and competed baseline and efficient
+      # results, if applicable
+      if (uncertainty == TRUE){
+        lines(years, eff_uc_l, lwd=2, lty=3, col=plot_col_uc_lowhigh)
+        lines(years, eff_uc_h, lwd=2, lty=3, col=plot_col_uc_lowhigh)
+        lines(years, eff_c_l, lwd=2, lty=3, col=plot_col_c_lowhigh)
+        lines(years, eff_c_h, lwd=2, lty=3, col=plot_col_c_lowhigh)
       }
-
+      # Add mean uncompeted efficient results
+      lines(years, eff_uc_m, lwd=3, col=plot_col_uc_eff)
+      # Add competed baseline results
+      lines(years, base_c, lwd=3.5, col=plot_col_c_base)
+      # Add mean competed efficient results
+      lines(years, eff_c_m, lwd=2, col=plot_col_c_eff)
+      
       # Add x and y axis labels
       mtext("Year", side=1, line=3.5, cex=0.925)
       mtext(plot_axis_labels[v], side=2, line=3.75, cex=0.925)
@@ -275,12 +257,13 @@ for (v in 1:length(var_names_uncompete)){
       axis(side=3, at=pretty(c(min(years), max(years))), labels = NA)
       axis(side=4, at=ylims, labels = NA)
     }
-
+  
     # Add plot legend
     par(xpd=TRUE)
     plot(1, type="n", axes=F, xlab="", ylab="") # creates blank plot square for legend
     legend("top", legend=legend_param, lwd=lwd_param, col=col_param, lty=lty_param, 
-           bty="n", border = FALSE, merge = TRUE, cex=1.15)
-  dev.off()
+       bty="n", border = FALSE, merge = TRUE, cex=1.15)
+    # Close plot device
+    dev.off()
   }
 }
