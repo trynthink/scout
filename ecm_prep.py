7e0c0b24dd069a2a32b21896f693217751d3b823
Peter DeWitt <peter.dewitt@nrel.gov>
2021-11-22 12:37:56 -0700

    add optional --alt_regions_breakdown argument for ecm_prep.py
    This argument is ignored if --alt_regions is not specified.  The default
    value (0) is set such that the existing interactive specification of the
    alternative breakdown is retained.  Specifying a value 1 (EMM), 2
    (state), or 3 (AIA climate zone) will allow the user to script what was
    an interactive step.

--- ecm_prep.py
+++ ecm_prep.py
@@ -10836,7 +10836,7 @@ def main(base_dir):
     # than the AIA climate zones, prompt the user to directly select that
     # alternate regional breakout (NEMS EMM or State)
     if opts and opts.alt_regions is True:
-        input_var = 0
+        input_var = str(opts.alt_regions_breakdown)
         # Determine the regional breakdown to use (NEMS EMM (1) vs. State (2)
         # vs. AIA (3))
         while input_var not in ['1', '2', '3']:
@@ -11669,6 +11669,13 @@ if __name__ == "__main__":
     # Optional flag for non-AIA regional breakdown
     parser.add_argument("--alt_regions", action="store_true",
                         help="Flag alternate regional breakdown")
+    # Optional flag for alternate regional breakdown.
+    parser.add_argument("--alt_regions_breakdown", type = int, default = 0,
+                        help = "This option is ignored if --alt_regions is not specificed."
+                        " 0 (default) for interactive specification of regional breakdonw."
+                        " 1 use an EIA NEMS Electricity Market Model (EMM)"
+                        " 2 use a state geograhpical breakdown"
+                        " 3 use an AIA climate zome geograhpical breakdown")
     # Optional flag for TSV metrics
     parser.add_argument("--tsv_metrics", action="store_true",
                         help="Flag time sensitive valuation metrics")
