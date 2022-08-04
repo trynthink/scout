b58fa24a3c988aaa0cb470eb7947f736af7a9056
Jared Langevin <jared.langevin@gmail.com>
2022-08-04 06:44:56 -0400

    Address merge commits when pulling in GCAM stock breakouts commit

--- run_test.py
+++ run_test.py
@@ -11970,14 +11970,14 @@ class ResCompeteTest(unittest.TestCase, CommonMethods):
                 "Technical potential"]["competed"]["mseg_out_break"] = {
                 key: copy.deepcopy(m.markets["Technical potential"][
                     "competed"]["mseg_out_break"])
-                for key in ["energy", "carbon", "cost"]
+                for key in ["stock", "energy", "carbon", "cost"]
             }
             # Extend for max. adoption potential breakouts
             cls.measures_all[ind].markets[
                 "Max adoption potential"]["competed"]["mseg_out_break"] = {
                 key: copy.deepcopy(m.markets["Max adoption potential"][
                     "competed"]["mseg_out_break"])
-                for key in ["energy", "carbon", "cost"]
+                for key in ["stock", "energy", "carbon", "cost"]
             }
         cls.measures_demand = cls.measures_all[0:2]
         cls.measures_supply = cls.measures_all[2:5]
@@ -12132,14 +12132,14 @@ class ResCompeteTest(unittest.TestCase, CommonMethods):
                 "Technical potential"]["competed"]["mseg_out_break"] = {
                 key: copy.deepcopy(m.markets["Technical potential"][
                     "competed"]["mseg_out_break"])
-                for key in ["energy", "carbon", "cost"]
+                for key in ["stock", "energy", "carbon", "cost"]
             }
             # Extend for max. adoption potential breakouts
             cls.measures_all_dist[ind].markets[
                 "Max adoption potential"]["competed"]["mseg_out_break"] = {
                 key: copy.deepcopy(m.markets["Max adoption potential"][
                     "competed"]["mseg_out_break"])
-                for key in ["energy", "carbon", "cost"]
+                for key in ["stock", "energy", "carbon", "cost"]
             }
         cls.measures_demand_dist = cls.measures_all_dist[0:2]
         cls.measures_supply_dist = cls.measures_all_dist[2:5]
@@ -22436,14 +22436,14 @@ class ComCompeteTest(unittest.TestCase, CommonMethods):
                 "Technical potential"]["competed"]["mseg_out_break"] = {
                 key: copy.deepcopy(m.markets["Technical potential"][
                     "competed"]["mseg_out_break"])
-                for key in ["energy", "carbon", "cost"]
+                for key in ["stock", "energy", "carbon", "cost"]
             }
             # Extend for max. adoption potential breakouts
             cls.measures_all[ind].markets[
                 "Max adoption potential"]["competed"]["mseg_out_break"] = {
                 key: copy.deepcopy(m.markets["Max adoption potential"][
                     "competed"]["mseg_out_break"])
-                for key in ["energy", "carbon", "cost"]
+                for key in ["stock", "energy", "carbon", "cost"]
             }
         cls.measures_secondary = [cls.measures_all[1]]
         # Instantiate engine object based on above measures
@@ -22603,14 +22603,14 @@ class ComCompeteTest(unittest.TestCase, CommonMethods):
                 "Technical potential"]["competed"]["mseg_out_break"] = {
                 key: copy.deepcopy(m.markets["Technical potential"][
                     "competed"]["mseg_out_break"])
-                for key in ["energy", "carbon", "cost"]
+                for key in ["stock", "energy", "carbon", "cost"]
             }
             # Extend for max. adoption potential breakouts
             cls.measures_all_dist[ind].markets[
                 "Max adoption potential"]["competed"]["mseg_out_break"] = {
                 key: copy.deepcopy(m.markets["Max adoption potential"][
                     "competed"]["mseg_out_break"])
-                for key in ["energy", "carbon", "cost"]
+                for key in ["stock", "energy", "carbon", "cost"]
             }
         cls.measures_secondary_dist = [cls.measures_all_dist[1]]
         cls.a_run_dist = run.Engine(
