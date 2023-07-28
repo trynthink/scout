04768486abddf003c92270c24e17ec4497c02de3
Jared Langevin <jared.langevin@gmail.com>
2023-07-28 14:13:21 -0400

    Fix issues preventing GCAM scenario data reporting
    The code was not properly checking/accomodating Scout output breakout categories to ensure they could be mapped to the categories needed for GCAM reporting data. Additionally, GCAM reporting requires stock units, but these were not being pulled. Both issues prevented any modification of reference case data to reflect measure impacts in the GCAM reporting format. These issues were fixed.

--- tests/run_test.py
+++ tests/run_test.py
@@ -346,7 +346,8 @@ class TestMeasureInit(unittest.TestCase, Constants):
     @classmethod
     def setUpClass(cls):
         """Define objects/variables for use across all class functions."""
-        handyvars = run.UsefulVars(Constants.HANDYFILES, gcam_out=False)
+        handyvars = run.UsefulVars(Constants.HANDYFILES, gcam_out=False,
+                                   brkout="basic")
         cls.sample_measure = CommonTestMeasures().sample_measure
         measure_instance = run.Measure(handyvars, **cls.sample_measure)
         cls.attribute_dict = measure_instance.__dict__
@@ -375,7 +376,8 @@ class OutputBreakoutDictWalkTest(unittest.TestCase, CommonMethods, Constants):
     @classmethod
     def setUpClass(cls):
         """Define objects/variables for use across all class functions."""
-        handyvars = run.UsefulVars(Constants.HANDYFILES, gcam_out=False)
+        handyvars = run.UsefulVars(Constants.HANDYFILES, gcam_out=False,
+                                   brkout="basic")
         cls.focus_yrs_test = handyvars.aeo_years
         sample_measure = CommonTestMeasures().sample_measure
         measure_list = [run.Measure(handyvars, **sample_measure)]
@@ -475,7 +477,8 @@ class PrioritizationMetricsTest(unittest.TestCase, CommonMethods, Constants):
     @classmethod
     def setUpClass(cls):
         """Define objects/variables for use across all class functions."""
-        cls.handyvars = run.UsefulVars(Constants.HANDYFILES, gcam_out=False)
+        cls.handyvars = run.UsefulVars(Constants.HANDYFILES, gcam_out=False,
+                                       brkout="basic")
         # Hardcode adjustments to common cost year to 1
         cls.handyvars.cost_convert = {"stock": 1, "energy": 1, "carbon": 1}
         cls.sample_measure_res = CommonTestMeasures().sample_measure4
@@ -1448,7 +1451,8 @@ class MetricUpdateTest(unittest.TestCase, CommonMethods, Constants):
     @classmethod
     def setUpClass(cls):
         """Define objects/variables for use across all class functions."""
-        cls.handyvars = run.UsefulVars(Constants.HANDYFILES, gcam_out=False)
+        cls.handyvars = run.UsefulVars(Constants.HANDYFILES, gcam_out=False,
+                                       brkout="basic")
         sample_measure = CommonTestMeasures().sample_measure4
         cls.measure_list = [run.Measure(cls.handyvars, **sample_measure)]
         cls.ok_base_life = 3
@@ -1508,7 +1512,8 @@ class PaybackTest(unittest.TestCase, Constants):
     @classmethod
     def setUpClass(cls):
         """Define objects/variables for use across all class functions."""
-        cls.handyvars = run.UsefulVars(Constants.HANDYFILES, gcam_out=False)
+        cls.handyvars = run.UsefulVars(Constants.HANDYFILES, gcam_out=False,
+                                       brkout="basic")
         sample_measure = CommonTestMeasures().sample_measure
         cls.measure_list = [run.Measure(cls.handyvars, **sample_measure)]
         cls.ok_cashflows = [[-10, 1, 1, 1, 1, 5, 7, 8], [-10, 14, 2, 3, 4],
@@ -1591,7 +1596,8 @@ class ResCompeteTest(unittest.TestCase, CommonMethods, Constants):
     @classmethod
     def setUpClass(cls):
         """Define objects/variables for use across all class functions."""
-        cls.handyvars = run.UsefulVars(Constants.HANDYFILES, gcam_out=False)
+        cls.handyvars = run.UsefulVars(Constants.HANDYFILES, gcam_out=False,
+                                       brkout="basic")
         # Reset meta retro rate
         cls.handyvars.retro_rate = {yr: 0 for yr in cls.handyvars.aeo_years}
         cls.test_adopt_scheme = "Technical potential"
@@ -16155,7 +16161,8 @@ class ComCompeteTest(unittest.TestCase, CommonMethods, Constants):
     @classmethod
     def setUpClass(cls):
         """Define objects/variables for use across all class functions."""
-        cls.handyvars = run.UsefulVars(Constants.HANDYFILES, gcam_out=False)
+        cls.handyvars = run.UsefulVars(Constants.HANDYFILES, gcam_out=False,
+                                       brkout="basic")
         # Reset meta retro rate
         cls.handyvars.retro_rate = {yr: 0 for yr in cls.handyvars.aeo_years}
         cls.test_adopt_scheme = "Technical potential"
@@ -26638,7 +26645,8 @@ class NumpyConversionTest(unittest.TestCase, CommonMethods, Constants):
     @classmethod
     def setUpClass(cls):
         """Define objects/variables for use across all class functions."""
-        cls.handyvars = run.UsefulVars(Constants.HANDYFILES, gcam_out=False)
+        cls.handyvars = run.UsefulVars(Constants.HANDYFILES, gcam_out=False,
+                                       brkout="basic")
         cls.sample_measure = {
             "market_entry_year": None,
             "market_exit_year": None,
@@ -26692,7 +26700,8 @@ class AddedSubMktFractionsTest(unittest.TestCase, CommonMethods, Constants):
     @classmethod
     def setUpClass(cls):
         """Define objects/variables for use across all class functions."""
-        cls.handyvars = run.UsefulVars(Constants.HANDYFILES, gcam_out=False)
+        cls.handyvars = run.UsefulVars(Constants.HANDYFILES, gcam_out=False,
+                                       brkout="basic")
         # Set standard adoption schemes
         cls.handyvars.adopt_schemes = [
             "Technical potential", "Max adoption potential"]
