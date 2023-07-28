9296856d4b7727b14010014e2d8112a552576a0b
Jared Langevin <jared.langevin@gmail.com>
2023-07-28 14:13:21 -0400

    Fix issues preventing GCAM scenario data reporting
    The code was not properly checking/accomodating Scout output breakout categories to ensure they could be mapped to the categories needed for GCAM reporting data. Additionally, GCAM reporting requires stock units, but these were not being pulled. Both issues prevented any modification of reference case data to reflect measure impacts in the GCAM reporting format. These issues were fixed.

--- run_test.py
+++ run_test.py
@@ -342,7 +342,8 @@ class TestMeasureInit(unittest.TestCase):
             "fossil_equivalent", "NA", "NA", "NA", "NA"], regions="AIA",
             grid_decarb=False)
         handyfiles.glob_vars = "glob_run_vars_test.json"
-        handyvars = run.UsefulVars(base_dir, handyfiles, gcam_out=False)
+        handyvars = run.UsefulVars(
+            base_dir, handyfiles, gcam_out=False, brkout="basic")
         cls.sample_measure = CommonTestMeasures().sample_measure
         measure_instance = run.Measure(handyvars, **cls.sample_measure)
         cls.attribute_dict = measure_instance.__dict__
@@ -376,7 +377,8 @@ class OutputBreakoutDictWalkTest(unittest.TestCase, CommonMethods):
             "fossil_equivalent", "NA", "NA", "NA", "NA"], regions="AIA",
             grid_decarb=False)
         handyfiles.glob_vars = "glob_run_vars_test.json"
-        handyvars = run.UsefulVars(base_dir, handyfiles, gcam_out=False)
+        handyvars = run.UsefulVars(
+            base_dir, handyfiles, gcam_out=False, brkout="basic")
         cls.focus_yrs_test = handyvars.aeo_years
         sample_measure = CommonTestMeasures().sample_measure
         measure_list = [run.Measure(handyvars, **sample_measure)]
@@ -481,7 +483,8 @@ class PrioritizationMetricsTest(unittest.TestCase, CommonMethods):
             "fossil_equivalent", "NA", "NA", "NA", "NA"], regions="AIA",
             grid_decarb=False)
         handyfiles.glob_vars = "glob_run_vars_test.json"
-        cls.handyvars = run.UsefulVars(base_dir, handyfiles, gcam_out=False)
+        cls.handyvars = run.UsefulVars(
+            base_dir, handyfiles, gcam_out=False, brkout="basic")
         # Hardcode adjustments to common cost year to 1
         cls.handyvars.cost_convert = {"stock": 1, "energy": 1, "carbon": 1}
         cls.sample_measure_res = CommonTestMeasures().sample_measure4
@@ -1459,7 +1462,8 @@ class MetricUpdateTest(unittest.TestCase, CommonMethods):
             "fossil_equivalent", "NA", "NA", "NA", "NA"], regions="AIA",
             grid_decarb=False)
         handyfiles.glob_vars = "glob_run_vars_test.json"
-        cls.handyvars = run.UsefulVars(base_dir, handyfiles, gcam_out=False)
+        cls.handyvars = run.UsefulVars(
+            base_dir, handyfiles, gcam_out=False, brkout="basic")
         sample_measure = CommonTestMeasures().sample_measure4
         cls.measure_list = [run.Measure(cls.handyvars, **sample_measure)]
         cls.ok_base_life = 3
@@ -1524,7 +1528,8 @@ class PaybackTest(unittest.TestCase):
             "fossil_equivalent", "NA", "NA", "NA", "NA"], regions="AIA",
             grid_decarb=False)
         handyfiles.glob_vars = "glob_run_vars_test.json"
-        cls.handyvars = run.UsefulVars(base_dir, handyfiles, gcam_out=False)
+        cls.handyvars = run.UsefulVars(
+            base_dir, handyfiles, gcam_out=False, brkout="basic")
         sample_measure = CommonTestMeasures().sample_measure
         cls.measure_list = [run.Measure(cls.handyvars, **sample_measure)]
         cls.ok_cashflows = [[-10, 1, 1, 1, 1, 5, 7, 8], [-10, 14, 2, 3, 4],
@@ -1612,7 +1617,8 @@ class ResCompeteTest(unittest.TestCase, CommonMethods):
             "fossil_equivalent", "NA", "NA", "NA", "NA"], regions="AIA",
             grid_decarb=False)
         handyfiles.glob_vars = "glob_run_vars_test.json"
-        cls.handyvars = run.UsefulVars(base_dir, handyfiles, gcam_out=False)
+        cls.handyvars = run.UsefulVars(
+            base_dir, handyfiles, gcam_out=False, brkout="basic")
         # Reset meta retro rate
         cls.handyvars.retro_rate = {yr: 0 for yr in cls.handyvars.aeo_years}
         cls.test_adopt_scheme = "Technical potential"
@@ -16174,7 +16180,8 @@ class ComCompeteTest(unittest.TestCase, CommonMethods):
             "fossil_equivalent", "NA", "NA", "NA", "NA"], regions="AIA",
             grid_decarb=False)
         handyfiles.glob_vars = "glob_run_vars_test.json"
-        cls.handyvars = run.UsefulVars(base_dir, handyfiles, gcam_out=False)
+        cls.handyvars = run.UsefulVars(
+            base_dir, handyfiles, gcam_out=False, brkout="basic")
         # Reset meta retro rate
         cls.handyvars.retro_rate = {yr: 0 for yr in cls.handyvars.aeo_years}
         cls.test_adopt_scheme = "Technical potential"
@@ -26658,7 +26665,8 @@ class NumpyConversionTest(unittest.TestCase, CommonMethods):
             "fossil_equivalent", "NA", "NA", "NA", "NA"], regions="AIA",
             grid_decarb=False)
         handyfiles.glob_vars = "glob_run_vars_test.json"
-        cls.handyvars = run.UsefulVars(base_dir, handyfiles, gcam_out=False)
+        cls.handyvars = run.UsefulVars(
+            base_dir, handyfiles, gcam_out=False, brkout="basic")
         cls.sample_measure = {
             "market_entry_year": None,
             "market_exit_year": None,
@@ -26717,7 +26725,8 @@ class AddedSubMktFractionsTest(unittest.TestCase, CommonMethods):
             "fossil_equivalent", "NA", "NA", "NA", "NA"], regions="AIA",
             grid_decarb=False)
         handyfiles.glob_vars = "glob_run_vars_test.json"
-        cls.handyvars = run.UsefulVars(base_dir, handyfiles, gcam_out=False)
+        cls.handyvars = run.UsefulVars(
+            base_dir, handyfiles, gcam_out=False, brkout="basic")
         # Set standard adoption schemes
         cls.handyvars.adopt_schemes = [
             "Technical potential", "Max adoption potential"]
