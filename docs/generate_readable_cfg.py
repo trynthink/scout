14c57c55124858b9af811249d3af554a4329bd02
aspeake1 <andrew.speake@nrel.gov>
2024-01-26 17:09:30 -0700

    Check for valid readable config file in CI

--- docs/generate_readable_cfg.py
+++ docs/generate_readable_cfg.py
@@ -2,10 +2,7 @@ from scout.config import Config
 from scout.constants import FilePaths as fp
 from pathlib import Path
 import yaml
-
-schema_file = fp.SUPPORTING_DATA / "config_schema.yml"
-schema_data = Config._load_config(schema_file)
-output_file = Path(__file__).resolve().parents[0] / "config_readable.yml"
+import argparse
 
 
 def convert_yaml_structure(schema_data: dict, required: list = [], parent_key: str = None) -> dict:
@@ -40,15 +37,21 @@ def convert_yaml_structure(schema_data: dict, required: list = [], parent_key: s
                 dtype.remove("null")
                 dtype = dtype[0]
             default = value.get("default")
+            if not default:
+                default = "null"
             enum = value.get("enum")
             arr_enum = value.get("items", {}).get("enum")
             enum_txt = ""
             # If applicable, update description for allowable string enumerations
             if enum:
-                enum_txt = f" Allowed values are {set(enum)}."
+                enum = sorted(filter(None, enum)) + ["null"] * enum.count(None)
+                enum_txt = f" Allowed values are {{{', '.join(str(x) for x in enum)}}}."
             # If applicable, update description for allowable array enumerations
             if dtype == "array" and arr_enum:
-                enum_txt = f" Allowed values are 0 or more of {set(arr_enum)}"
+                arr_enum = sorted(filter(None, arr_enum)) + ["null"] * arr_enum.count(None)
+                enum_txt = (
+                    f" Allowed values are 0 or more of {{{', '.join(str(x) for x in arr_enum)}}}."
+                )
             req_txt = ""
             if key in required:
                 req_txt = ", required"
@@ -61,6 +64,27 @@ def convert_yaml_structure(schema_data: dict, required: list = [], parent_key: s
     return output_yaml
 
 
-output_cfg = convert_yaml_structure(schema_data["properties"], (schema_data["required"], None))
-with open(output_file, "w") as file:
-    yaml.dump(output_cfg, file, default_flow_style=False, width=60)
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        description="Convert schema file data to a more readable version for documentation."
+    )
+    parser.add_argument(
+        "-o",
+        "--output_file",
+        type=str,
+        help="Path to the output file. If not provided, the output will be printed to the console.",
+    )
+    args = parser.parse_args()
+
+    schema_file = fp.SUPPORTING_DATA / "config_schema.yml"
+    schema_data = Config._load_config(schema_file)
+    output_file = args.output_file
+    if args.output_file:
+        output_file = Path(args.output_file)
+    else:
+        output_file = Path(__file__).resolve().parent / "config_readable.yml"
+
+    output_cfg = convert_yaml_structure(schema_data["properties"], (schema_data["required"], None))
+    with open(output_file, "w") as file:
+        yaml.dump(output_cfg, file, default_flow_style=False, width=60)
