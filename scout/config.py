85b3afb40e54aababf54aadf09305edaaa18a560
Jared Langevin <jtlangevin@users.noreply.github.com>
2024-03-12 11:38:03 -0400

    Continue ECM processing with errors, check all ECM inputs
    Through try/except handling, errors preparing individual ECMs or ECM packages are now captured and written out to a text file in the ./generated folder. Skipped ECMs are also now registered to a new list (“skipped”) in ./generated/run_setup.json, and users are warned if there are any ECMs in this list when executing run.py. Fixes #255.
    
    Additionally, pulled check_mkt_inputs() function out of fill_mkts() such that it is executed across all ECMs in the site before individual ECM markets are prepared. Fixes #369.
    
    Modified the filter_invalid_packages() function to be more general, pulling the user warning out of the function and the corresponding test.

--- scout/config.py
+++ scout/config.py
@@ -17,7 +17,7 @@ class LogConfig:
         logging.basicConfig(
             level=logging.INFO,
             format="%(asctime)s - %(levelname)s: %(message)s",
-            datefmt="%d-%b-%y %H:%M:%S",
+            datefmt="%Y-%m-%d %H:%M:%S",
         )
 
 
