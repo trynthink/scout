cd04799d550ed68cf2abb79af51a9841f0a5c2fa
Jared Langevin <jtlangevin@users.noreply.github.com>
2024-07-19 11:26:13 -0400

    Improve robustness of check for variable existence
    Replace assumption that first loop in ECM markets is always when perf/cost/lifetime/mkt scaling fractions need to be initialized.

--- scout/ecm_prep.py
+++ scout/ecm_prep.py
@@ -2900,21 +2900,23 @@ class Measure(object):
                 contrib_mseg_key = tuple(contrib_mseg_key)
 
             # Initialize measure performance/cost/lifetime, associated units,
-            # and sub-market scaling fractions/sources if: a) For loop through
-            # all measure mseg key chains is in first iteration, b) A switch
-            # has been made from updating "primary" microsegment info. to
-            # updating "secondary" microsegment, and c) Any of performance/
-            # cost/lifetime units is a dict which must be parsed further to
-            # reach the final value. Additionally, for cost/cost units only,
-            # initialize if a) Incentives information has been applied, b) $/sf
-            # is used as cost units, c) Switching from one end use to another,
-            # or from one building vintage to another * Note: cost/lifetime/
-            # sub-market info. is not updated for "secondary" microsegments,
-            # which do not pertain to these variables; lifetime units in years
-            if ind == 0 or (ms_iterable[ind][0] != ms_iterable[ind - 1][0]) \
+            # and sub-market scaling fractions/sources if: a) The variable has
+            # not yet been initialized at all, b) A switch has been made from
+            # updating "primary" microsegment info. to updating "secondary"
+            # microsegment, and c) Any of performance/ cost/lifetime units is a
+            # dict which must be parsed further to reach the final value.
+            # Additionally, for cost/cost units only, initialize if a)
+            # Incentives information has been applied, b) $/sf is used as cost
+            # units, c) Switching from one end use to another, or from one
+            # building vintage to another * Note: cost/lifetime/ sub-market
+            # info. is not updated for "secondary" microsegments, which do not
+            # pertain to these variables; lifetime units in years
+            if ('perf_meas' not in locals()) or (
+                ms_iterable[ind][0] != ms_iterable[ind - 1][0]) \
                or isinstance(self.energy_efficiency, dict):
                 perf_meas = self.energy_efficiency
-            if ind == 0 or (ms_iterable[ind][0] != ms_iterable[ind - 1][0]) \
+            if ('perf_units' not in locals()) or (
+                ms_iterable[ind][0] != ms_iterable[ind - 1][0]) \
                or isinstance(self.energy_efficiency_units, dict):
                 perf_units = self.energy_efficiency_units
             if mskeys[0] == "secondary":
@@ -2941,7 +2943,8 @@ class Measure(object):
                 # to another, or original cost/cost units are in dict format;
                 # reset cost/cost units to those of original measure
                 elif ((sqft_subst == 1 or
-                      "$/ft^2 floor" in self.cost_units) or ind == 0 or (
+                      "$/ft^2 floor" in self.cost_units) or (
+                        'cost_meas' not in locals()) or (
                         ms_iterable[ind][0] != ms_iterable[ind - 1][0]) or (
                         ms_iterable[ind][4] != ms_iterable[ind - 1][4]) or (
                         ms_iterable[ind][-1] != ms_iterable[ind - 1][-1]) or
@@ -2950,14 +2953,14 @@ class Measure(object):
                     cost_units, cost_meas = [
                         self.cost_units, self.installed_cost]
                 # Set lifetime attribute to initial value
-                if ind == 0 or isinstance(
-                        self.product_lifetime, dict):
+                if ('life_meas' not in locals()) or \
+                        isinstance(self.product_lifetime, dict):
                     life_meas = self.product_lifetime
                 # Set market scaling attributes to initial values
-                if ind == 0 or isinstance(
-                        self.market_scaling_fractions, dict):
+                if ('mkt_scale_frac' not in locals()) \
+                        or isinstance(self.market_scaling_fractions, dict):
                     mkt_scale_frac = self.market_scaling_fractions
-                if ind == 0 or isinstance(
+                if ('mkt_scale_frac_source' not in locals()) or isinstance(
                         self.market_scaling_fractions_source, dict):
                     mkt_scale_frac_source = \
                         self.market_scaling_fractions_source
@@ -5880,10 +5883,12 @@ class Measure(object):
                                             x in mskeys[-2] for x in [
                                             "coal", "kerosene"]])):
                                         out_fuel_save = f[0]
-                                    # Assign commercial other fuel to Distillate/Other
+                                    # Assign commercial other fuel to
+                                    # Distillate/Other
                                     elif f[0] == "Distillate/Other" and (
                                         mskeys[2] in
-                                            self.handyvars.in_all_map['bldg_type']['commercial']):
+                                            self.handyvars.in_all_map[
+                                            'bldg_type']['commercial']):
                                         out_fuel_save = f[0]
                                     # Assign wood tech.
                                     elif f[0] == "Biomass" and (
