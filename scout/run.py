cbf6a39e45edb83e86d2ce42a350b1c947e5bdaf
aspeake1 <andrew.speake@nrel.gov>
2024-10-22 16:13:52 -0600

    test commit for CI

--- scout/run.py
+++ scout/run.py
@@ -5458,8 +5458,8 @@ def main(opts: argparse.NameSpace):  # noqa: F821
             data = round(data, precision)
         return data
 
-    a_run.output_ecms = round_values(a_run.output_ecms, 6)
-    a_run.output_all = round_values(a_run.output_all, 6)
+    a_run.output_ecms = round_values(a_run.output_ecms, 3)
+    a_run.output_all = round_values(a_run.output_all, 3)
 
     # Write summary outputs for individual measures to a JSON
     with open(handyfiles.meas_engine_out_ecms, "w") as jso:
