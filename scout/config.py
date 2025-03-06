84439fd14fca93f3a3c011db547d62926072ea58
aspeake1 <andrew.speake@nrel.gov>
2025-03-05 17:39:10 -0700

    Breakout ecm_prep shared methods and variable classes in separate modules.

--- scout/config.py
+++ scout/config.py
@@ -8,6 +8,66 @@ from pathlib import Path
 from jsonschema import validate
 
 
+class ImmutableDict:
+    def __init__(self, d):
+        self._dict = d
+
+    def __getitem__(self, key):
+        return self._dict[key]
+
+    def __iter__(self):
+        return iter(self._dict)
+
+    def __len__(self):
+        return len(self._dict)
+
+    def __contains__(self, key):
+        return key in self._dict
+
+    def items(self):
+        return self._dict.items()
+
+    def keys(self):
+        return self._dict.keys()
+
+    def values(self):
+        return self._dict.values()
+
+    def __repr__(self):
+        return repr(self._dict)
+
+    def __str__(self):
+        return str(self._dict)
+
+    def __eq__(self, other):
+        if isinstance(other, ImmutableDict):
+            return self._dict == other._dict
+        return self._dict == other
+
+    def __setitem__(self, key, value):
+        raise ValueError(f"Cannot modify ImmutableDict: {key}")
+
+    def __delitem__(self, key):
+        raise ValueError("Cannot modify ImmutableDict")
+
+    def clear(self):
+        raise ValueError("Cannot modify ImmutableDict")
+
+    def pop(self, key, default=None):
+        raise ValueError("Cannot modify ImmutableDict")
+
+    def popitem(self):
+        raise ValueError("Cannot modify ImmutableDict")
+
+    def setdefault(self, key, default=None):
+        raise ValueError("Cannot modify ImmutableDict")
+
+    def update(self, *args, **kwargs):
+        raise ValueError("Cannot modify ImmutableDict")
+
+    def to_dict(self):
+        return dict(self._dict)
+
 class LogConfig:
     """Configure the logger
     """
